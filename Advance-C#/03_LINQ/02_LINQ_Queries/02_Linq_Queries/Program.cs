e51c7b56aa1c60a0ab84ca925bb42a95a163194c
Aalam <aalamansari1712@gmail.com>
2025-02-16 14:50:31 +0530

    added linq learning

---
+++ Advance-C#/03_LINQ/02_LINQ_Queries/02_Linq_Queries/Program.cs
@@ -0,0 +1,193 @@
+﻿using Extensions;
+using Repositories.Entities;
+using Services;
+using System.ComponentModel.Design;
+using System.Net;
+
+namespace LINQ_Queries
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<Employee> employeeList = Data.GetEmployees();
+            List<Department> departmentList = Data.GetDepartments();
+
+            // using select - Method Syntax
+            var res1 = employeeList.Select(e => new
+            {
+                FullName = e.FirstName + " " + e.LastName,
+                AnnualSalary = e.AnnualSalary
+            });
+
+            foreach (var item in res1)
+            {
+                Console.WriteLine(item.FullName);
+                Console.WriteLine(item.AnnualSalary);
+            }
+
+            Console.WriteLine("====================1====================");
+
+            // using select with where
+            var res2 = employeeList.Select(e => new
+            {
+                FullName = e.FirstName + " " + e.LastName,
+                AnnualSalary = e.AnnualSalary
+            }).Where(e => e.AnnualSalary > 20000);
+
+            foreach (var item in res2)
+            {
+                Console.WriteLine(item.FullName);
+                Console.WriteLine(item.AnnualSalary);
+            }
+
+            Console.WriteLine("====================2====================");
+
+            // using from and where - Query Syntax
+            var res3 = from emp in employeeList
+                       where emp.AnnualSalary > 2000
+                       select new
+                       {
+                           FullName = emp.FirstName + " " + emp.LastName,
+                           AnnualSalary = emp.AnnualSalary
+                       };
+
+            foreach (var item in res3)
+            {
+                Console.WriteLine($"{item.FullName} {item.AnnualSalary}");
+            }
+
+            Console.WriteLine("====================3====================");
+
+            // Query syntax get executed once we iterate over the resultant object
+            var res4 = from emp in employeeList.GetHigSalariedEmployees()
+                       select new
+                       {
+                           FullName = emp.FirstName + " " + emp.LastName,
+                           AnnualSalary = emp.AnnualSalary
+                       };
+
+            // we're adding this later still it gets included in the resultant object coz of Query syntax
+            employeeList.Add(new Employee
+            {
+                Id = 5,
+                FirstName = "Asad",
+                LastName = "Sayyed",
+                AnnualSalary = 100000.23m,
+                IsManager = true,
+                DepartmentId = 2
+            });
+
+            foreach (var item in res4)
+            {
+                Console.WriteLine($"{item.FullName} {item.AnnualSalary}");
+            }
+
+            Console.WriteLine("====================4====================");
+
+            // Immediate execution using ToList()
+            var res5 = (from emp in employeeList.GetHigSalariedEmployees()
+                        select new
+                        {
+                            FullName = emp.FirstName + " " + emp.LastName,
+                            AnnualSalary = emp.AnnualSalary
+                        }).ToList();
+
+            // Now this won't be included in the resultant object as we're performing immediate execution
+            employeeList.Add(new Employee
+            {
+                Id = 6,
+                FirstName = "Awaiz",
+                LastName = "Khan",
+                AnnualSalary = 100000.23m,
+                IsManager = true,
+                DepartmentId = 2
+            });
+
+            foreach (var item in res5)
+            {
+                Console.WriteLine($"{item.FullName} {item.AnnualSalary}");
+            }
+
+            Console.WriteLine("====================5====================");
+
+            // Join - Method Syntax
+            var joinRes1 = departmentList.Join(employeeList,
+                    department => department.Id,
+                    employee => employee.DepartmentId,
+                    (department, employee) => new
+                    {
+                        FullName = employee.FirstName + " " + employee.LastName,
+                        AnnualSalary = employee.AnnualSalary,
+                        DepartmentName = department.LongName
+                    }
+                );
+
+            foreach (var item in joinRes1)
+            {
+                Console.WriteLine($"{item.FullName} {item.AnnualSalary} {item.DepartmentName}");
+            }
+
+            Console.WriteLine("====================6====================");
+
+            // Join - Query Syntax
+            var joinRes2 = from dept in departmentList
+                           join emp in employeeList
+                           on dept.Id equals emp.DepartmentId
+                           select new
+                           {
+                               FullName = emp.FirstName + " " + emp.LastName,
+                               AnnualSalary = emp.AnnualSalary,
+                               DepartmentName = dept.LongName
+                           };
+
+            foreach (var item in joinRes2)
+            {
+                Console.WriteLine($"{item.FullName} {item.AnnualSalary} {item.DepartmentName}");
+            }
+
+            Console.WriteLine("====================7====================");
+
+            // GroupJoin (Left Join) - Method syntax
+            var joinRes3 = departmentList.GroupJoin(employeeList,
+                dept => dept.Id,
+                emp => emp.DepartmentId,
+                (dept, emp) => new          // This tuple can consist of any variables, they are just representation of the resultant object
+                {
+                    Employees = emp,
+                    DepartmentName = dept.LongName
+                });
+            
+            foreach (var item in joinRes3)
+            {
+                Console.WriteLine($"{item.DepartmentName}");
+                foreach (var item2 in item.Employees)
+                {
+                    Console.WriteLine($"{item2.FirstName} {item2.LastName}");
+                }
+            }
+
+            Console.WriteLine("====================8====================");
+
+            // GroupJoin (Left Join) - Query syntax
+            var joinRes4 = from dept in departmentList
+                           join emp in employeeList
+                           on dept.Id equals emp.DepartmentId
+                           into employeeGroup
+                           select new
+                           {
+                               Employees = employeeGroup,
+                               DepartmentName = dept.LongName,
+                           };
+
+            foreach (var item in joinRes4)
+            {
+                Console.WriteLine($"{item.DepartmentName}");
+                foreach (var item2 in item.Employees)
+                {
+                    Console.WriteLine($"{item2.FirstName} {item2.LastName}");
+                }
+            }
+        }
+    }
+}
---
+++ Advance-C#/03_LINQ/03_LINQ_Operators/Services/Data.cs
@@ -0,0 +1,109 @@
+﻿using Repositories.Entities;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public static class Data
+    {
+        public static List<Employee> GetEmployees()
+        {
+            List<Employee> employees = new List<Employee>();
+            Employee employee = new Employee
+            {
+                Id = 1,
+                FirstName = "Bob",
+                LastName = "Janes",
+                AnnualSalary = 4000.12m,
+                IsManager = true,   
+                DepartmentId = 3,   
+            };
+            employees.Add(employee);
+
+            employee = new Employee
+            {
+                Id = 2,
+                FirstName = "Jana",
+                LastName = "Fox",
+                AnnualSalary = 2220.12m,
+                IsManager = false,
+                DepartmentId = 1,
+            };
+            employees.Add(employee);
+
+            employee = new Employee
+            {
+                Id = 3,
+                FirstName = "Yogesh",
+                LastName = "Mittal",
+                AnnualSalary = 223.34m,
+                IsManager = false,
+                DepartmentId = 3,
+            };
+            employees.Add(employee);
+
+            employee = new Employee
+            {
+                Id = 4,
+                FirstName = "Jane",
+                LastName = "Smith",
+                AnnualSalary = 40492.4m,
+                IsManager = true,
+                DepartmentId = 2,
+            };
+            employees.Add(employee);
+
+            return employees;   
+        }
+
+        public static List<Department> GetDepartments()
+        {
+            List<Department> departments = new List<Department>();
+            Department department = new Department
+            {
+                Id = 1,
+                ShortName = "HR",
+                LongName = "Human Resources"
+            };
+            departments.Add(department);
+
+            department = new Department
+            {
+                Id = 2,
+                ShortName = "FN",
+                LongName = "Finance"
+            };
+            departments.Add(department);
+
+            department = new Department
+            {
+                Id = 3,
+                ShortName = "TE",
+                LongName = "Technology"
+            };
+            departments.Add(department);
+
+            return departments;
+        }
+
+        public static ArrayList GetHetreogenousDataCollection()
+        {
+            ArrayList arrayList = new ArrayList();
+            arrayList.Add(100);
+            arrayList.Add("Bob James");
+            arrayList.Add(1000);
+            arrayList.Add(2000);
+            arrayList.Add(new Employee { Id = 6, FirstName = "Jackie", LastName = "Chan", AnnualSalary = 13032, IsManager = false, DepartmentId = 1 });
+            arrayList.Add(new Employee { Id = 6, FirstName = "Naruto", LastName = "Uzumaki", AnnualSalary = 65743, IsManager = true, DepartmentId = 2 });
+            arrayList.Add(new Department { Id = 4, ShortName = "LO", LongName = "Logistics" });
+            arrayList.Add(new Department { Id = 5, ShortName = "BA", LongName = "Banking" });
+
+            return arrayList;
+        }
+
+    }
+}
